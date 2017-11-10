dc9e179d8d82ce201155a6f269d9df97059921d3
iregon <0iregon0@gmail.com>
2017-11-10 18:53:48 +0100

    Bug fix

--- src/ClientFrame.java
+++ src/ClientFrame.java
@@ -1,22 +1,110 @@
+import java.awt.*;
+import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 
-public class ClientFrame extends JFrame implements Observer{
+/** Chat client UI */
+class ClientFrame extends JFrame implements Observer {
 	
-	private static final long serialVersionUID = 1L;
-
-	public ClientFrame() {
-		super("Client");
-		
-		setVisible(true);
-		setLocation(20, 20);
-		setSize(100, 100);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
-
-	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
-		
-	}
+	private JTextArea textArea;
+	private JTextField inputTextField;
+	private JButton sendButton;
+	private JPanel p_mainPanel;
+	private ChatAccess chatAccess;
 	
+    public ClientFrame(ChatAccess chatAccess) {
+        this.chatAccess = chatAccess;
+        chatAccess.addObserver(this);
+        buildGUI();
+    }
+
+    /** Builds the user interface */
+    private void buildGUI() {
+    	Container c = this.getContentPane();
+    	
+    	p_mainPanel = new JPanel();
+    	p_mainPanel.setLayout(new BorderLayout());
+    	
+    	c.add(p_mainPanel);
+    	
+        textArea = new JTextArea(20, 50);
+        textArea.setEditable(false);
+        textArea.setLineWrap(true);
+        p_mainPanel.add(new JScrollPane(textArea), BorderLayout.CENTER);
+
+        Box box = Box.createHorizontalBox();
+        p_mainPanel.add(box, BorderLayout.SOUTH);
+        inputTextField = new JTextField();
+        sendButton = new JButton("Send");
+        box.add(inputTextField);
+        box.add(sendButton);
+
+        // Action for the inputTextField and the goButton
+        ActionListener sendListener = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String str = inputTextField.getText();
+                if (str != null && str.trim().length() > 0)
+                    chatAccess.send(str);
+                inputTextField.selectAll();
+                inputTextField.requestFocus();
+                inputTextField.setText("");
+            }
+        };
+        inputTextField.addActionListener(sendListener);
+        sendButton.addActionListener(sendListener);
+
+        this.addWindowListener(new WindowListener() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                chatAccess.close();
+            }
+
+			@Override
+			public void windowOpened(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowClosed(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowIconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowDeiconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowActivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowDeactivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+        });
+    }
+
+    /** Updates the UI depending on the Object argument */
+    public void update(Observable o, Object arg) {
+        final Object finalArg = arg;
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                textArea.append(finalArg.toString());
+                textArea.append("\n");
+            }
+        });
+    }
 }
